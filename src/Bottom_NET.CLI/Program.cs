ea5ecc8bd5255122dae3b1f863778f5a1385d653
Josh <josh.ja.butt@gmail.com>
2021-01-21 21:10:05 +1000

    Inital Release

---
+++ src/Bottom_NET.CLI/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Text;
+
+namespace Bottom_NET.CLI
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            RootCommand rootCommand = new RootCommand
+            {
+                new Option<bool>(
+                    new string[] {"-b", "--bottomify"},
+                    description: "Translate text to bottom"),
+               new Option<bool>(
+                    new string[] {"-r", "--regress"},
+                    description: "Translate bottom to human-readable text (futile)"),
+                new Option<FileInfo>(
+                    new string[] {"-i", "--input"},
+                    description: "Input file [default: stdin]"),
+               new Option<FileInfo>(
+                    new string[] {"-o", "--output"},
+                    description: "Output file [default: stdout]"),
+               new Argument<string>(
+                    "text",
+                    getDefaultValue: () => null
+               )
+            };
+
+            rootCommand.Description = "Fantastic (maybe) CLI for translating between bottom and human-readable text";
+
+            rootCommand.Handler = CommandHandler.Create<bool, bool, FileInfo, FileInfo, string>((bottomify, regress, input, output, text) =>
+            {
+                if (!(bottomify || regress))
+                {
+                    Console.Error.WriteLine("TODO.");
+                    return;
+                }
+
+                if (bottomify && regress)
+                {
+                    Console.Error.WriteLine("TODO.");
+                    return;
+                }
+
+                if (input is null && text is null)
+                {
+                    Console.Error.WriteLine("Either input text or the --input options must be provided.");
+                    return;
+                }
+
+                if (!(input is null)) {
+                    if (!input.Exists)
+                    {
+                        Console.Error.WriteLine($"Input file \"{input.FullName}\" does not exist.");
+                        return;
+                    }
+                    text = input.OpenText().ReadToEnd();
+                }
+
+                string result;
+                if (bottomify)
+                {
+                    result = Bottom.encode_string(text);
+                }
+                else
+                {
+                    result = Bottom.decode_string(text);
+                }
+
+                if (!(output is null))
+                {
+                    using StreamWriter sw = output.CreateText();
+                    sw.Write(result);
+                } 
+                else
+                {
+                    Console.WriteLine(result);
+                }
+
+            });
+
+            return rootCommand.InvokeAsync(args).Result;
+        }
+    }
+}
---
+++ src/Bottom_Net/Bottom.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bottom_NET
+{
+    public static class Bottom
+    {
+        private const string LINE_ENDING = "👉👈";
+
+        private static readonly Dictionary<byte, string> _character_values = new Dictionary<byte, string>()
+        {
+            {200, "🫂"},
+            {50, "💖"},
+            {10, "✨"},
+            {5, "🥺"},
+            {1, ","},
+            {0, "❤️"}
+        };
+
+        private static readonly Dictionary<byte, string> _byte_to_emoji = MapByteToEmoji();
+        private static readonly Dictionary<string, byte> _emoji_to_byte = MapEmojiToByte();
+
+        # region Public methods
+
+        public static string encode_byte(byte value)
+        {
+            return _byte_to_emoji[value] + LINE_ENDING;
+        }
+
+
+        public static byte decode_byte(string input) 
+        {
+            if (_emoji_to_byte.ContainsKey(input))
+            {
+                return _emoji_to_byte[input];
+            }
+            throw new KeyNotFoundException($"Cannot decode character {input}");
+        }
+
+        public static string encode_string(string input)
+        {
+            return string.Join("", Encoding.UTF8.GetBytes(input).Select(encode_byte).ToArray());
+        }
+
+        public static string decode_string(string input)
+        {
+            string[] chars = input.Split(new string[] { "\u200B", LINE_ENDING }, StringSplitOptions.RemoveEmptyEntries);
+            return Encoding.UTF8.GetString(chars.Select(decode_byte).ToArray());
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static string ByteToEmoji(byte value)
+        {
+            StringBuilder buffer = new StringBuilder();
+
+            if (value == 0)
+            {
+                return _character_values[0];
+            }
+
+            while (value > 0)
+            {
+                string to_push = string.Empty;
+                byte subtract_by = 0;
+
+                foreach (KeyValuePair<byte, string> mapping in _character_values)
+                {
+                    if (value >= mapping.Key)
+                    {
+                        to_push = mapping.Value;
+                        subtract_by = mapping.Key;
+                        goto push;
+                    }
+                }
+
+                push:;
+                buffer.Append(to_push);
+                value -= subtract_by;
+            }
+            return buffer.ToString();
+        }
+
+        private static Dictionary<byte, string> MapByteToEmoji()
+        {
+            Dictionary<byte, string> mapping = new Dictionary<byte, string>();
+
+            byte i = 0;
+            do
+            {
+                mapping[i] = ByteToEmoji(i);
+                i++;
+            } while (i != 0);
+
+            return mapping;
+        }
+
+        private static Dictionary<string, byte> MapEmojiToByte()
+        {
+            Dictionary<string, byte> mapping = new Dictionary<string, byte>();
+
+            foreach (KeyValuePair<byte, string> map in _byte_to_emoji)
+            {
+                mapping[map.Value] = map.Key;
+            }
+
+            return mapping;
+        }
+
+        #endregion
+
+    }
+}
